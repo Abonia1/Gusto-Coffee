c22d8eb022a47626bb48cce8a2fbffd95fc168ec
Abonia Sojasingarayar <aboniaa@gmail.com>
2019-05-29 16:43:06 +0200

    added number of person and  avalability check

--- reservation.php
+++ reservation.php
@@ -9,86 +9,144 @@
     <link rel="stylesheet" href="jquery-ui.css">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>jQuery UI Datepicker - Default functionality</title>
     <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
     <link rel="stylesheet" href="/resources/demos/style.css">
     <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
     <script src="js/timepicker.js"></script>
-
-
-
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-timepicker/1.8.1/jquery.timepicker.min.css"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-timepicker/1.8.1/jquery.timepicker.min.js"></script>
 
+
     <!-- <link rel="stylesheet" type="text/css" href="jquery.datetimepicker.css"/>
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-<script src="jquery.datetimepicker.js"></script> -->
-
-
-
-
+    <script src="jquery.datetimepicker.js"></script> -->
     <script>
         $(function() {
             $("#datepicker").datepicker();
         });
         $(document).ready(function() {
-            $('.txt').timepicker();
+            $('.timepick').timepicker({
+                timeFormat: 'H:i',
+                dropdown: true,
+                scrollbar: true,
+                minTime: '10:00',
+                maxTime: '18:00',
+            });
         });
-    </script>
-
-    <!-- <script>
-
-$('#demo').datetimepicker({
-
-inline:true,
-
+    
+    //try 1
+    
+    jQuery(document).ready(function(){
+    // This button will increment the value
+    $('.qtyplus').click(function(e){
+        // Stop acting like a button
+        e.preventDefault();
+        // Get the field name
+        fieldName = $(this).attr('field');
+        // Get its current value
+        var currentVal = parseInt($('input[name='+fieldName+']').val());
+        // If is not undefined
+        if (!isNaN(currentVal)) {
+            // Increment
+            $('input[name='+fieldName+']').val(currentVal + 1);
+        } else {
+            // Otherwise put a 0 there
+            $('input[name='+fieldName+']').val(0);
+        }
+    });
+    // This button will decrement the value till 0
+    $(".qtyminus").click(function(e) {
+        // Stop acting like a button
+        e.preventDefault();
+        // Get the field name
+        fieldName = $(this).attr('field');
+        // Get its current value
+        var currentVal = parseInt($('input[name='+fieldName+']').val());
+        // If it isn't undefined or its greater than 0
+        if (!isNaN(currentVal) && currentVal > 0) {
+            // Decrement one
+            $('input[name='+fieldName+']').val(currentVal - 1);
+        } else {
+            // Otherwise put a 0 there
+            $('input[name='+fieldName+']').val(0);
+        }
+    });
 });
 
-    </script>-->
 
 
+//to call show() function to display the plan image
+function show() {
+  //document.getElementById('planImg').style.maxHeight = "200px";
+  document.getElementById('planImg').src="IMAGES/plan2.png";
+  
+}
 
+</script>
+    <!-- <script>
+        $(document).ready(function() {
+            var timepicker = new TimePicker('time', {
+                lang: 'en',
+                theme: 'dark'
+            });
+            timepicker.on('change', function(evt) {
 
+                var value = (evt.hour || '00' );
+                evt.element.value = value;
+
+            });
+        });
+    </script> -->
+
+    <!-- <script>
+$('#demo').datetimepicker({
+inline:true,
+});
+</script>-->
 </head>
 
 <body>
     <?php
     include('menu.php');
     ?>
+    
+        <div class="container" style="height:100%">
 
 
+            <table  style="border-spacing: 40px;">
+                <tr>
+                    <td><label>Date</label>
+                        <input type="text" id="datepicker" required="true" />
+                    </td>
 
-    <div class="container" style="height:100%">
-        <form action="thankyou.php" method="post">
+                    <td>
+                        <label>Time</label>
+                        <input class="timepick" style="position:relative"/>
+                    </td>
+                    <td style="padding-top:20px;">
+                        <label>People</label>
+                        <input type='button' value='-' class='qtyminus' field='quantity' />
+    <input type='text' name='quantity' value='0' class='qty' />
+    <input type='button' value='+' class='qtyplus' field='quantity' />
+                        
+                    </td>
+                    <td></td>
+                    <td>
+                    <button class="button" onclick="show()">Check Availability</button>
+</td>
 
-            <div class="col-md-6" style="width:50%;">
-                <label>Date</label>
-                <input type="text" id="datepicker" required="true">
-            </div>
-            <div class="col-md-6" style="width:50%;float:right">
-                <label>Time</label>
-                <input class="txt">
-                <svg width="50%" height="80%">
-                    <rect x="50" y="20" rx="20" ry="20" width="50%" height="80%" style="fill:red;stroke:black;stroke-width:5;opacity:0.5" />
-                </svg>
-            </div>
+                </tr>
+            </table>
+           
+            <img id="planImg"">
 
-
-
-
-
-    </div>
-
-
-
-
+        </div>
+        
+   
     <?php
     include('footer.php');
     ?>
-
-
-
 </body>
 
 </html>
