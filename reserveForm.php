e2af2a3bd3d04dfe4602666feaabe0adf26e2560
Abonia Sojasingarayar <aboniaa@gmail.com>
2019-06-18 14:29:38 +0200

    form submission

--- reserveForm.php
+++ reserveForm.php
@@ -1,30 +1,50 @@
 <html>
+    <head>
+        <title>
+        </title>
+        <link rel="stylesheet" type="text/css" href="css/menu.css" />
+        <link rel="stylesheet" type="text/css" href="css/reservation.css" />
+    </head>
+    <body>
+        <?php
+$conn=mysqli_connect("localhost","root","","gustocoffee");
+// Check connection
+if($conn === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+//mysqli_select_db("gustocoffee");
+if(isset($_POST['Submit'])){
+    $a=$_POST['DateForm'];
+    $b=$_POST['TimeForm'];
+    $c=$_POST['quantity'];
+    $d=$_POST['Name'];
+    $e=$_POST['EMail'];
+    $f=$_POST['Telephone'];
+    $g=$_POST['Address'];
+    $h=$_POST['Comment'];
+    //$sql="INSERT INTO `reservation` (`reservation_id`, `date`, `time`, `quantity`, `name`, `email`, `telephone`, `adress`, `comment`) VALUES (``, `$a`, `$b`, `$c`, `$d`, `$e`, `$f`, `$g`, `$h`)";
+    $stmt = mysqli_prepare($conn, "INSERT INTO `reservation` (`date`,`time`,`quantity`,`name`,`email`,`telephone`,`adress`,`comment`) VALUES (?, ?, ?, ?, ?, ?, ?,?)");
+    mysqli_stmt_bind_param($stmt, 'ssississ', $a, $b, $c, $d, $e, $f, $g, $h);
+    /* execute prepared statement */
+    mysqli_stmt_execute($stmt);
+    //printf("%d Row inserted.\n", mysqli_stmt_affected_rows($stmt));
+    /* close statement and connection */
+    mysqli_stmt_close($stmt);
 
-<head>
-    <title></title>
-    <link rel="stylesheet" type="text/css" href="css/menu.css" />
-    <link rel="stylesheet" type="text/css" href="css/reservation.css" />
-</head>
-
-<body>
-    <?php
-				include ('menu.php');
-				?>
-    <div class="Reserve">
-
-
-        <p style="font-family:Vladimir Script;font-size:38px;">Thank you for reserving your seat in advance.See you soon
-        </p>
-
-    </div>
-
-    <?php
-				        include ('footer.php');
-				    ?>
-
-
-
-
-</body>
-
+    mysqli_close($conn);
+ 
+}
+include ('menu.php');
+        ?>
+        <div class="Reserve">
+            <p style="font-family:Vladimir Script;font-size:38px;">
+                Thank you 
+                <?php echo $_POST["Name"]; ?> for reserving
+                your seat in advance.See you Soon
+            </p>
+        </div>
+        <?php
+include ('footer.php');
+        ?>
+    </body>
 </html>
